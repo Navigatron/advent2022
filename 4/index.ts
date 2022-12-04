7b0903b3cfbce413cf521127cfa43f0dabcb7107
Ethan Witherington <etw9578@rit.edu>
2022-12-04 02:26:20 -0500

    code style checks

--- 4/index.ts
+++ 4/index.ts
@@ -1,49 +1,36 @@
 
-type Range = {start: number, end: number};
+// A range has a start and an end
+type Range = {start: number; end: number};
+
+// A pair is a ... pair of ranges
 type Pair = [Range, Range];
 
+// Convert input string to pairs of ranges
 function parsePairs(input: string): Pair[] {
-	return input.trim().split('\n').map((line: string)=>{
-		// return two ranges
-		return line.split(',').map((range: string)=>{
-			const [start, end] = range.split('-').map(num=>parseInt(num) as number) as [number, number];
+	// Process by line
+	return input.trim().split('\n').map(line =>
+		// Process by range
+		line.split(',').map(range => {
+			// Convert to Range type
+			const [start, end] = range.split('-').map(n => Number.parseInt(n, 10)) as [number, number];
 			return {start, end};
-		}) as [Range, Range];
-	});
+		}) as [Range, Range],
+	);
 }
 
-export function part1(input: string){
-	// each section has an ID
-	// each elf gets a range of IDs.
-	// elf ranges overlap?
-	// some ranges fully contain the other
-	// // in how many pairs does one fully contain the other?
-
+export function part1(input: string) {
 	const pairs: Pair[] = parsePairs(input);
-
-	const containingPairs = pairs.filter(pair=>{
-		// Does one of these contain the other?
-		// case 1 - first is bigger
-		return (pair[0].start <= pair[1].start && pair[0].end >= pair[1].end) || (pair[0].start >= pair[1].start && pair[0].end <= pair[1].end);
-	});
-	
+	const containingPairs = pairs.filter(pair =>
+		// If p0 (contains) or (is contained by) p1, count it.
+		(pair[0].start <= pair[1].start && pair[0].end >= pair[1].end)
+		|| (pair[0].start >= pair[1].start && pair[0].end <= pair[1].end),
+	);
 	return containingPairs.length;
 }
 
-export function part2(input: string){
+export function part2(input: string) {
 	const pairs = parsePairs(input);
-
-	// Do it overlap?
-	const overlapping = pairs.filter(pair=>{
-		// return true if overlap a contains b start, or a contains b end
-		const p0 = pair[0];
-		const p1 = pair[1];
-
-		return (p0.start <= p1.end && p0.end >= p1.start);
-		
-	});
-
+	// Line overlap test
+	const overlapping = pairs.filter(p => (p[0].start <= p[1].end && p[0].end >= p[1].start));
 	return overlapping.length;
-	// 715 is too low!
-	
 }
